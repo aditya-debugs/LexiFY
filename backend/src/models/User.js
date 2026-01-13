6f08107cc8b5b80e8522104ce33149c7765560d7
aditya-debugs <adityaofficial1641@gmail.com>
2026-01-14 04:58:55 +0530

    Create User model with authentication fields

---
+++ backend/src/models/User.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";   
+
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+},
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,   
+        required: true,
+        minlength: 6,
+    },
+    bio: {
+        type: String,
+        default: "",
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+    nativeLanguage: {
+        type: String,
+        default: "",
+    },
+    learningLanguage: {
+        type: String,
+        default: "",
+    },
+    location: {
+        type: String,
+        default: "",
+    },
+    isOnboarded: {
+        type: Boolean,
+        default: false,
+    },
+
+    friends: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ],
+    
+    // Daily Word Streak tracking
+    streakCount: {
+        type: Number,
+        default: 0,
+    },
+    lastStreakDate: {
+        type: Date,
+        default: null,
+    },
+    
+    // Close friends for Daily Word visibility
+    closeFriends: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ],
+},{timeStamp:true});
+
+
+
+
+userSchema.pre("save",async function(next) {
+    if (!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    }catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.matchPassword = async function(enterPassword) {
+    const isPasswordCorrect = await bcrypt.compare(enterPassword, this.password);
+    return isPasswordCorrect;
+};
+
+const User = mongoose.model("User", userSchema);
+
+
+
+export default User;
